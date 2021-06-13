2f7b36c99f7b76b4b1753550b8d614b77e9ea48c
AnushaManda-27 <anushamanda27@gmail.com>
2021-06-13 18:08:47 +0530

    Added: CSV data and Reader

---
+++ CSVReader.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+    public static void main(String[] args){
+
+        String path = "C:\\Users\\M Veeraiah\\csv file\\entry.csv";
+        String line = " ";
+        {
+            try
+            {
+                BufferedReader bReader = new BufferedReader(new FileReader(path));
+
+                while((line = bReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+            } catch (FileNotFoundException e) {
+                
+                e.printStackTrace();
+            } catch (IOException e1) {
+                
+                e1.printStackTrace();
+            }
+        }
+    }
+}
+
